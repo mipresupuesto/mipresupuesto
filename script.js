b1411023081a0887426bb9004f0c57f095c380bc
unknown <Isabel@DESKTOP-MHUIUD9.confipetrol.com>
2023-05-16 19:07:52 -0500

    Cambio de var a const y ajuste para mobile

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ var total = 0;
 
 		function agregarIngreso() {
             // parseInt Convierte un argumento de tipo cadena y devuelve un entero de la base especificada.
-            var descripcion = (document.getElementById("descripcion__Ingresos").value);
-			var valor = parseInt(document.getElementById("valor_Ingresos").value);
+            const descripcion = (document.getElementById("descripcion__Ingresos").value);
+			const valor = parseInt(document.getElementById("valor_Ingresos").value);
             
             
 			// Crear nueva fila en la tabla
-			var tablaValores = document.getElementById("tabla_Ingresos");
-			var nuevaFila = tablaValores.insertRow();
-			var celdaDescripcion = nuevaFila.insertCell(0);
-            var celdaValor = nuevaFila.insertCell(1);
-            var celdaTotal = nuevaFila.insertCell(2);
+			const tablaValores = document.getElementById("tabla_Ingresos");
+			const nuevaFila = tablaValores.insertRow();
+			const celdaDescripcion = nuevaFila.insertCell(0);
+            const celdaValor = nuevaFila.insertCell(1);
+            const celdaTotal = nuevaFila.insertCell(2);
 
 			// Actualizar celda con valor ingresado
 			celdaValor.innerHTML = valor;
@@ -30,15 +30,15 @@ var total = 0;
 
 		
 		function agregarGasto() {
-			var descripcion2 = (document.getElementById("descripcion__Gastos").value);
-            var valor2 = parseInt(document.getElementById("valor_Gastos").value);
+			const descripcion2 = (document.getElementById("descripcion__Gastos").value);
+            const valor2 = parseInt(document.getElementById("valor_Gastos").value);
 
 			// Crear nueva fila en la tabla
-			var tablaValores = document.getElementById("tabla_Gastos");
-			var nuevaFila = tablaValores.insertRow();
-			var celdaDescripcion2 = nuevaFila.insertCell(0);
-            var celdaValor2 = nuevaFila.insertCell(1)
-			var celdaTotal = nuevaFila.insertCell(2);
+			const tablaValores = document.getElementById("tabla_Gastos");
+			const nuevaFila = tablaValores.insertRow();
+			const celdaDescripcion2 = nuevaFila.insertCell(0);
+            const celdaValor2 = nuevaFila.insertCell(1)
+			const celdaTotal = nuevaFila.insertCell(2);
 
 			// Actualizar celda con valor ingresado
 			celdaDescripcion2.innerHTML = descripcion2;
@@ -55,11 +55,11 @@ var total = 0;
 
         function restar() {
         // obtener total de ingresos y gastos
-        var totalIngresos = parseInt(document.getElementById("total_Ingresos").innerHTML);
-        var totalGastos = parseInt(document.getElementById("total_Gastos").innerHTML);
+        const totalIngresos = parseInt(document.getElementById("total_Ingresos").innerHTML);
+        const totalGastos = parseInt(document.getElementById("total_Gastos").innerHTML);
 
         // calcular balance y actualizar celda correspondiente
-        var balance = totalIngresos - totalGastos;
+        const balance = totalIngresos - totalGastos;
         document.getElementById("balance").innerHTML = balance;
 
         // actualizar celda de total de gastos en balance
